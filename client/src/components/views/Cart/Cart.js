4a71ddc39ebc1c61fbd506a1709084cf590e58da
ChiZhang0907 <chizhang0907@gmail.com>
2020-08-26 02:00:25 -0700

    Cart Feature

--- client/src/components/views/Cart/Cart.js
+++ client/src/components/views/Cart/Cart.js
@@ -1,10 +1,24 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {getCartItems} from '../../../_actions/user_actions'
 import CartCard from './Sections/CartCard'
+import {Result, Empty} from 'antd'
 
 function Cart (props) {
+
     const dispatch = useDispatch();
+
+    const [price, setPrice] = useState(0)
+
+    const calculatePrice = (cartDetail) => {
+        let total = 0
+
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        })
+
+        setPrice(total)
+    }
 
     useEffect (() => {
         let cartItems = []
@@ -18,14 +32,30 @@ function Cart (props) {
         }
     }, [props.user.userData])
 
+    useEffect(() => {
+        if(props.user.cartDetail && props.user.cartDetail.length > 0) {
+            calculatePrice(props.user.cartDetail)
+        }
+    }, [props.user.cartDetail])
+
     return (
         <div style={{width: '85%', margin: '3rem auto'}}>
             <h1>My Cart</h1>
             
             <div>
-                <CartCard>
-                    
-                </CartCard>
+                <CartCard products={props.user.cartDetail}></CartCard>
+
+                <div style={{marginTop: '3rem'}}>
+                    <h2>Total amount: $ {price}</h2>
+                </div>
+
+                <Result status="success" title="Successfully Purchased Items"></Result>
+
+                <div style={{width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <br></br>
+                    <Empty description={false}></Empty>
+                    <p>No Items In the Cart</p>
+                </div>
             </div>
         </div>
     )
--- client/src/components/views/Cart/Sections/CartCard.js
+++ client/src/components/views/Cart/Sections/CartCard.js
@@ -1,11 +1,44 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
 function CartCard (props) {
    
+    const handleImage = (images) => {
+        if(images.length > 0) {
+            let image = images[0]
+            return `http://localhost:5000/${image}`
+        }
+    }
+
+    const renderItems = () => (
+        props.products && props.products.map(product => (
+            <tr key={product._id}>
+                <td>
+                    <img style={{ width: '70px' }} alt="product" src={handleImage(product.images)}></img>
+                </td> 
+                <td style={{fontFamily: 'Helvetica'}}>{product.quantity} EA</td>
+                <td style={{fontFamily: 'Helvetica'}}>$ {product.price} </td>
+                <td style={{fontFamily: 'Helvetica'}}>
+                    <button onClick>Remove</button> 
+                </td>
+            </tr>
+        ))
+    )
 
     return (
         <div>
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{fontFamily: 'Helvetica'}}>Product Image</th>
+                        <th style={{fontFamily: 'Helvetica'}}>Product Quantity</th>
+                        <th style={{fontFamily: 'Helvetica'}}>Product Price</th>
+                        <th style={{fontFamily: 'Helvetica'}}>Remove from Cart</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderItems()}
+                </tbody>
+            </table>
         </div>
     )
 }
